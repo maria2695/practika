23c1351f1fdcf2055b58c950877fb73236980857
Maria Lazareva <m.lazareva20040625@gmail.com>
2023-07-06 19:21:43 +0300

    Empty

--- demo/src/main/java/com/example/demo/repository/DrinkRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entities.Drink;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DrinkRepository extends JpaRepository<Drink, Long>  {
-}
--- demo/src/main/java/com/example/demo/repository/StaffRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entities.Staff;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StaffRepository extends JpaRepository<Staff, Long>  {
-}
